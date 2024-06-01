6d516c7fe61b0eaf807a40730a7d7d5044f31fea
nicola gasparro <40263928+nicogaucho@users.noreply.github.com>
2024-06-01 10:26:59 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>react template</h1>;
+}
+
+export default App;
